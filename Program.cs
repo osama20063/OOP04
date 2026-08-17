99b5526ae3bff1c32998c653654161d11f6ee07c
osama ahmed <oa9064626@gmail.com>
2026-08-18 01:21:00 +0300

    part1 q 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Collections;
+using System.Numerics;
 using System.Timers;
 using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -40,28 +41,53 @@ namespace Assignment_oop_4
 
 
 
-//            b) Static Binding vs.Dynamic Binding
+            //            b) Static Binding vs.Dynamic Binding
 
-//Static Binding(Early Binding):
+            //Static Binding(Early Binding):
 
-//Method call is resolved at compile - time.
+            //Method call is resolved at compile - time.
 
-//Based on the reference type of the object.
+            //Based on the reference type of the object.
 
-//Used with Method Overloading, static methods, and non -virtual methods.
+            //Used with Method Overloading, static methods, and non -virtual methods.
 
-//Offers faster execution performance.
+            //Offers faster execution performance.
 
-//Dynamic Binding (Late Binding):
+            //Dynamic Binding (Late Binding):
 
-//Method call is resolved at runtime.
+            //Method call is resolved at runtime.
 
-//Based on the actual object type created in memory.
+            //Based on the actual object type created in memory.
 
-//Used with Method Overriding and virtual methods.
+            //Used with Method Overriding and virtual methods.
 
-//Has a slight performance overhead due to runtime lookup.
+            //Has a slight performance overhead due to runtime lookup.
 
+
+            #endregion
+
+
+
+
+
+
+            #region Part 01 2
+
+
+//            (Q2: Sealed Classes and Methods):
+
+//a) Purpose of the sealed keyword when applied to a class:
+//The sealed keyword prevents other classes from inheriting from it.It is used to restrict inheritance, protect the class design, and improve security and compiler optimizations.
+
+//b) Difference between a sealed class and a sealed method:
+
+//Sealed Class: Prevents any class from inheriting/deriving from it(it cannot be a base/parent class).
+
+//Sealed Method: Prevents a derived class from further overriding that specific method.It can only be applied to a method that is already overriding a virtual method from a base class (using sealed override).
+
+//c) Can a sealed method be overridden? Why?
+//No, a sealed method cannot be overridden.
+//Why: The explicit purpose of applying the sealed modifier to an overridden method is to break the virtual chain and guarantee that no further derived classes can alter its implementation.
 
             #endregion
 
